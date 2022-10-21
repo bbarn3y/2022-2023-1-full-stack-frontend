291f93f2da6a5ad1ff396fdf0a20ebb5a17290e1
Barnabás Balázs <barnabas.balazs@impresign.com>
2022-10-21 10:23:32 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = '2022-2023-1-full-stack-frontend';
+}
